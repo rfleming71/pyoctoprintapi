c6ab440979ee73d8fd5df3512a1c3ee7df45ba93
Ryan Fleming <rfleming71@gmail.com>
2021-11-17 20:05:21 -0500

    Expose web cam settings

--- pyoctoprintapi/settings.py
+++ pyoctoprintapi/settings.py
@@ -1,4 +1,5 @@
 """ Setting classes """
+from urllib.parse import urlparse, urlunparse, urlencode, parse_qsl
 
 class TrackingSetting:
     """Tracking information and settings"""
@@ -37,3 +38,53 @@ class DiscoverySettings:
     @property
     def upnp_uuid(self):
         return self._raw.get("upnpUuid")
+
+class WebcamSettings:
+    """Webcam settings"""
+    def __init__(self, base_url: str, raw: dict):
+        self._base_url = base_url
+        self._raw = raw
+
+    @property
+    def bitrate(self) -> str:
+        return self._raw["bitrate"]
+
+    @property
+    def flip_horizontal(self) -> str:
+        return self._raw["flipH"]
+
+    @property
+    def flip_vertical(self) -> str:
+        return self._raw["flipV"]
+
+    @property
+    def rotate_90(self) -> str:
+        return self._raw["rotate90"]
+
+    @property
+    def snapshot_ssl_validation_enabled(self) -> bool:
+        return self._raw["snapshotSslValidation"]
+
+    @property
+    def internal_snapshot_url(self) -> str:
+        return self._raw["snapshotUrl"]
+
+    @property
+    def external_snapshot_url(self) -> str:
+        url = self.stream_url
+        parsed = list(urlparse(url))
+        parsed[2] = parsed[2].replace("//", "/")
+        query = dict(parse_qsl(parsed[4]))
+        query["action"] = "snapshot"
+        parsed[4] = urlencode(query)
+
+        return urlunparse(parsed)
+
+    @property
+    def stream_url(self) -> str:
+        stream_url = self._raw["streamUrl"]
+        return f"{self._base_url}{stream_url}"
+
+    @property
+    def enabled(self) -> bool:
+        return self._raw["webcamEnabled"]
