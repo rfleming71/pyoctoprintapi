2e4a59d0c11e22cae07df5923bfe8fc6319eaaac
Ryan Fleming <rfleming71@gmail.com>
2023-01-10 20:46:18 -0500

    Support overriden webcam settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,8 +1,8 @@
 """Test setting information API."""
 import json
 
-from pyoctoprintapi.settings import TrackingSetting, DiscoverySettings
-from fixtures import TEST_SETTINGS_TRACKING, TEST_SETTINGS_DISCOVERY
+from pyoctoprintapi.settings import TrackingSetting, WebcamSettings, DiscoverySettings
+from fixtures import TEST_SETTINGS_CAMERA, TEST_SETTINGS_CAMERA_1, TEST_SETTINGS_TRACKING, TEST_SETTINGS_DISCOVERY
 
 def test_tracking_setting_properties():
     tracking = TrackingSetting(json.loads(TEST_SETTINGS_TRACKING))
@@ -16,3 +16,24 @@ def test_discovery_settings_properties():
     assert tracking.public_host == "host"
     assert tracking.public_port == 80
     assert tracking.upnp_uuid == "436fc3ec-fc2e-4851-b289-eb17974aa706"
+
+def test_webcam_settings_properties():
+    tracking = WebcamSettings("https://foo.com:8080", json.loads(TEST_SETTINGS_CAMERA))
+    assert tracking.enabled 
+    assert tracking.bitrate == "10000k"
+    assert tracking.external_snapshot_url == "https://foo.com:8080/webcam/?action=snapshot"
+    assert tracking.stream_url == "https://foo.com:8080/webcam/?action=stream"
+
+def test_webcam_settings_2_properties():
+    tracking = WebcamSettings("https://foo.com:8080", json.loads(TEST_SETTINGS_CAMERA))
+    assert tracking.enabled 
+    assert tracking.bitrate == "10000k"
+    assert tracking.external_snapshot_url == "https://foo.com:8080/webcam/?action=snapshot"
+    assert tracking.stream_url == "https://foo.com:8080/webcam/?action=stream"
+
+def test_webcam_settings_2_properties():
+    tracking = WebcamSettings("https://foo.com:8080", json.loads(TEST_SETTINGS_CAMERA_1))
+    assert tracking.enabled 
+    assert tracking.bitrate == "10000k"
+    assert tracking.external_snapshot_url == "http://127.0.0.1:8000/webcam/?action=snapshot"
+    assert tracking.stream_url == "http://127.0.0.1:8000/webcam/?action=stream"
