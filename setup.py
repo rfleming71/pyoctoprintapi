9ed430ef24a661e260bce992a369cea6ec2edb87
Markus Ressel <mail@markusressel.de>
2023-03-26 16:47:19 +0200

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 setup(
     name="pyoctoprintapi",
     packages=["pyoctoprintapi"],
-    version="0.1.11",
+    version="0.1.12",
     description="An asynchronous Python library for communicating with the OctoPrint API",
     author="Ryan Fleming",
     author_email="rfleming71@users.noreply.github.com",
